986d6a1713cae72dbfbe7f4caf7f516adc073f5a
bhaktarj000 <bhaktarj000@mysbisd.org>
2024-12-10 10:08:55 -0600

    update lom

--- config.js
+++ config.js
@@ -2,8 +2,10 @@ const config = {
   challenge: true, // Set to true if you want to enable password protection.
   users: {
     // You can add multiple users by doing username: 'password'.
-    admin: "mk41",
-    babies: "bossbaby",
+    admin: "123456789",
+    bob: "bob",
+    passwordstealer: "yulittle",
+    bigcheese: "biggieissofat"
   },
 };
 
